74f3dd47272361b2c47bd6073b234ee26a3466b0
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-08-15 12:14:23 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { Home } from "./Sections/Home/Home";
 import { Projects } from "./Sections/Projects/Projects";
 import "./App.css";
 import Contact from "./Sections/Contact/Contact";
-import Navbar from "./Components/Navbar/Navbar";
 import About  from "./Sections/About/About";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      <Navbar />
+     
       <div className="main-content">
         <Home />
         <About />
