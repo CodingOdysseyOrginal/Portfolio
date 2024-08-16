1efc747690d83acef7d634801e5eaa406d35120d
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-08-16 13:51:34 +0100

    contact test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Home } from "./Sections/Home/Home";
 import { Projects } from "./Sections/Projects/Projects";
 import "./App.css";
-import Contact from "./Sections/Contact/Contact";
 import About  from "./Sections/About/About";
 import Footer from "./Components/Footer/Footer";
 import Nav from "./Components/Nav/Nav";
@@ -14,7 +13,7 @@ function App() {
         <Home />
         <About />
         <Projects />
-        <Contact />
+        
         <Footer />
       </div>
     </div>
